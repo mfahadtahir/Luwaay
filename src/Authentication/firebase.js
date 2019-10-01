cf62455f37e00e507bae5f4c1ef0eabd8c08eec5
mfahadtahir <muhammadfahad325302@gmail.com>
2019-10-01 19:36:21 +0500

    Update firebase.js

--- src/Authentication/firebase.js
+++ src/Authentication/firebase.js
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyBNSD7RalAUWZ-E7sTdf4V0wWVT2mEak6o",
+    apiKey: "",
     authDomain: "luwaay-store.firebaseapp.com",
-    databaseURL: "https://luwaay-store.firebaseio.com",
+    databaseURL: "",
     projectId: "luwaay-store",
     storageBucket: "",
-    messagingSenderId: "1063830693178",
-    appId: "1:1063830693178:web:2e61742ca5e02caf"
+    messagingSenderId: "",
+    appId: ""
 };
